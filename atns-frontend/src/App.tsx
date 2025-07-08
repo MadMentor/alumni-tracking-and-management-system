683be2df43fb2d990e5cc535dec5e1881831dbe9
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-08 09:10:59 +0545

    Changes made to better work with vercel

--- atns-frontend/src/App.tsx
+++ atns-frontend/src/App.tsx
@@ -3,16 +3,22 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Register from './pages/Register/Register';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import {Analytics} from "@vercel/analytics/react"
+import {SpeedInsights} from "@vercel/speed-insights/react"
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Header/>
-            <Routes>
-                <Route path="/register" element={<Register/>}/>
-            </Routes>
-            <Footer />
-        </Router>
+        <>
+            <Router>
+                <Header/>
+                <Routes>
+                    <Route path="/register" element={<Register/>}/>
+                </Routes>
+                <Footer/>
+            </Router>
+            <Analytics/>
+            <SpeedInsights/>
+        </>
     );
 };
 
